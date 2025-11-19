237b87c25da29e5e7d7b7046c729387f0b86fdca
Anvar <eandpiareinfinity@gmail.com>
2025-11-19 16:33:05 +0500

    1-dars

---
+++ 1. javascript ga kirish/javascript-1.js
@@ -0,0 +1,47 @@
+/* 
+Comment(sharh) berish
+
+CTRL+K=>C    comment berish
+CTRL+K =>U    comment ni olib tashlash
+
+Agar bittada ko’p qatorga comment berish kerak bo’lsa, belgilab yana shu shortcut lar ishlatiladi.
+
+
+SHIFT+ALT=>A  multiline comment ochish uchun shortcut
+CTRL+/   bir qatorli comment ochish uchun shortcut
+
+
+Better comments ning imkoniyatlari:
+
+! → Muhim / xatolik (qizil rangda ko‘rinadi)
+
+? → Savol / tushunarsiz joy (ko‘k rangda ko‘rinadi)
+
+* → Ta’kid / muhim eslatma (yashil rangda ko‘rinadi)
+
+TODO → Bajarilishi kerak bo‘lgan ish (to‘q sariq rangda ko‘rinadi)
+
+
+*/
+
+/* 
+
+Extension:
+
+Javascript console utils   - console.log() larni olib tashlash uchun ishlatiladi
+
+Shortcut: CTRL+SHIFT+D
+
+console.log() haqida
+• Ma’lumotni konsolga(hech qanday UI siz) chiqaradi (brauzer dev tools da).
+• Dasturchi kodni tekshirish, o‘zgaruvchilar qiymatini ko‘rish, funksiyalar qanday ishlashini kuzatish uchun ishlatiladi.
+• Kodni yozishda xatoliklarni topish va tushunishni osonlashtiradi.
+
+Xulosa:
+• console.log() → kodni kuzatish va test qilish uchun vosita.
+• U foydalanuvchiga emas, balki dasturchiga mo‘ljallangan.
+• Frontend’da brauzer konsolida, backend’da esa Node.js terminalida natija ko‘rsatadi.
+
+
+
+*/
