92fa596d0407f191b620403d19925cc8ad4d50ce
Anvar <eandpiareinfinity@gmail.com>
2025-11-21 16:30:59 +0500

    Window va Document obyektlari

---
+++ 2. Javascript 2-dars/javascript-2.js
@@ -0,0 +1,56 @@
+//console.log(window); bu bizga window obyektini chiqaradi. window obyektining ichidagi barcha imkoniyatlarni console oynasida ko'rishimiz mumkin.
+
+// document.getElementById("ota").style.width = "100px";
+// document.getElementById("ota").style.height = "100px";
+// document.getElementById("ota").style.backgroundColor = "red";
+// Bu koddagi stil ishlaydi va boshqa stillardan ustun hisoblanadi.
+
+// Agar html faylga ham html fayl ichida inline, ham CSS faylda external, ham .js faylda stil beriladigan bo'lsa, .js faylidagi berilgan stil kuchliroq bo'ladi.
+
+// Document.getElementById("ota").style.width = "200px";
+// Document.getElementById("ota").style.height = "200px";
+// Document.getElementById("ota").style.backgroundColor = "green";
+// Bu koddagi stillar ishlamaydi, chunki bunda Document class iga stil berilgan. Odatda stil document obyektiga beriladi.
+
+// let ism = prompt("Sizni ismingiz nima?");
+// bunda browserimizda yangi modal oynasi chiqadi, ichida bitta textbox va OK, Cancel tugmalari bo'ladi. OK bosilsa, textbox dan kiritilgan ma'lumot ism o'zgaruvchisiga qabul qilinadi.
+
+// console.log(ism);
+// console.log(typeof ism);
+
+//---------------------------------------------
+// let javob = confirm("Saytda qolishni hohlaysizmi?");
+// Bunda hech qanday textbox bo'lmagan lekin OK va Cancel tugmalari bo'lgan modal oynasi chiqadi.
+
+// console.log(javob);  true yoki false qiymatini qaytaradi
+// console.log(typeof javob); turi boolean bo'ladi.
+
+// let ism = "Abror";
+
+// ism = "Sobirjon";
+// ism = "Shoxjaxon";
+
+// console.log(ism);
+
+// Agar let bilan e'lon qilsak, unga yana qiymat bera olamiz.
+
+//--------------------------------------
+
+// const odi = "Abror";
+
+// odi = "Sobirjon";
+// odi = "Shoxjaxon";
+
+// console.log(odi);
+
+// Agar const bilan e'lon qilsak, unga yana qiymat bera olmaymiz.
+
+//--------------------------------------
+
+// var nomi = "Suxrob";
+// nomi = "Asilbek";
+// nomi = "Hasanboy";
+
+// console.log(nomi);
+
+// var bilan e'lon qilsak, unga yana qiymat bera olamiz.
